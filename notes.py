a2e899f84e5a4857852bf6c0673b1c926650de16
irminaj <irminaj@github.com>
2022-11-08 23:19:49 +0200

    modify notes.py file

--- notes.py
+++ notes.py
@@ -4,6 +4,8 @@ from flask_bootstrap import Bootstrap5
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
 
+app.config['SECRET_KEY'] = 'SRKyycK9I35J1qzxPbKleSdDc4PreDlP'
+
 @app.route('/')
 def index():
     return render_template('index.html')
