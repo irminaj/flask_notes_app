061396c5e54f55d0fd0a918f159847ac9685c60e
irminaj <irminaj@github.com>
2022-11-09 20:19:05 +0200

    create simple login form and add it to the navbar

--- notes.py
+++ notes.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
+from wtforms import StringField, SubmitField, PasswordField, BooleanField
 from wtforms.validators import DataRequired, Email, EqualTo
 import email_validator
 
@@ -17,6 +17,12 @@ class RegistrationForm(FlaskForm):
     password2 = PasswordField('Confirm password', validators=[DataRequired()])
     submit = SubmitField('Register')
 
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Keep me logged in')
+    submit = SubmitField('Log In')
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -25,3 +31,8 @@ def index():
 def register():
     form = RegistrationForm()
     return render_template('register.html', form=form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', form=form)
