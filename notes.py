68245974eacaf40c715b941ee6c68b8f9134b91f
irminaj <irminaj@github.com>
2022-11-09 22:47:30 +0200

    define Category model

--- notes.py
+++ notes.py
@@ -55,6 +55,9 @@ class Note(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 class Category(db.Model):
+    __tablename__ = 'categories'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100), unique=True)
 
 
 
