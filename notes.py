454588c54ebbf253e1b3fe536defd8ad4dd0c405
irminaj <irminaj@github.com>
2022-11-12 00:01:33 +0200

    add password hashing in the User model

--- notes.py
+++ notes.py
@@ -9,6 +9,7 @@ import email_validator
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -43,8 +44,19 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
     email = db.Column(db.String(64), unique=True)
     username = db.Column(db.String(64), unique=True, index=True)
-    password = db.Column(db.String(128))
+    password_hash = db.Column(db.String(128))
     notes = db.relationship('Note')
+
+    @property
+    def password(self):
+        raise AttributeError("password is not a readable attribute")
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User %r>' % self.username
